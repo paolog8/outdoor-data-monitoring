d4be71b1b010ea718f294ce795add57218135b53
paolog8 <paolograniero8@gmail.com>
2026-07-30 12:06:20 +0200

    Add ON DELETE CASCADE to solar cell foreign key references

---
+++ migrations/V39__add_on_delete_cascade_to_solar_cell_references.sql
@@ -0,0 +1,29 @@
+-- cascade delete solar cell references when the referenced solar cell is deleted
+
+ALTER TABLE mpp_connection_event
+    DROP CONSTRAINT mpp_connection_event_solar_cell_id_fkey,
+    ADD CONSTRAINT mpp_connection_event_solar_cell_id_fkey
+        FOREIGN KEY (solar_cell_id)
+        REFERENCES solar_cell(id)
+        ON DELETE CASCADE;
+
+ALTER TABLE sensor_connection_event
+    DROP CONSTRAINT sensor_association_event_solar_cell_id_fkey,
+    ADD CONSTRAINT sensor_association_event_solar_cell_id_fkey
+        FOREIGN KEY (solar_cell_id)
+        REFERENCES solar_cell(id)
+        ON DELETE CASCADE;
+
+ALTER TABLE solar_cell_group
+    DROP CONSTRAINT solar_cell_group_cell_id_fkey,
+    ADD CONSTRAINT solar_cell_group_cell_id_fkey
+        FOREIGN KEY (solar_cell_id)
+        REFERENCES solar_cell(id)
+        ON DELETE CASCADE;
+
+ALTER TABLE solar_cell_experiment
+    DROP CONSTRAINT solar_cell_experiment_solar_cell_id_fkey,
+    ADD CONSTRAINT solar_cell_experiment_solar_cell_id_fkey
+        FOREIGN KEY (solar_cell_id)
+        REFERENCES solar_cell(id)
+        ON DELETE CASCADE;
